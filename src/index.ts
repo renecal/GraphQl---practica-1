24bba51938d6f2a6448dffdd3d9dd2419fd40f0b
Rene Calfin <rcalfino@emeal.nttdata.com>
2024-09-10 23:31:24 -0300

    add GraphQL schema and resolvers for User type

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log("hello word")
+//Tipo query
+const typeDefs  = `#graphql
+    type User {
+        name: String
+        email: String
+    }
+    type Query {
+        users: User
+    }
+`;
+
+// Dataset Users
+const users = [
+    {
+        name: 'John Doe',
+        email: 'test@test.cl',
+    },
+    {
+        name: 'Jane Doe',
+        email: 'test2@test2,cl',
+    }
+];
+
+// Resolvers
+const resolvers = {
+    Query: {
+        users: () => users[0]
+    }
+};
